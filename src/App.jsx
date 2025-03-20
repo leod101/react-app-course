457b1ef96e8c028bedda2e489c74a146a5b01e26
leod101 <leod060@gmail.com>
2025-03-20 21:40:10 +0200

    Signed-off-by: leod101 <leod060@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-
-import "./App.css";
-
+import React from "react";
+import MainLayout from "./components/MainLayout/MainLayout";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <MainLayout />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<div>home</div>} />
+          <Route path="/forbiden" element={<div>!!!!!!!!forbiden</div>} />
+          <Route path="/addquestion" element={<div> &&&&&&?????addquestion</div>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -1,14 +1,25 @@
 import React from "react";
+import cls from "./MainLayout.module.css";
+import { Outlet } from "react-router-dom";
+
+const currentYear = new Date().getFullYear();
 
 function MainLayout() {
   return (
-    <div>
-      <header>header</header>
-      <div>
-        <main>main</main>
-        <footer>footer</footer>
+    <>
+      <div className={cls.mainLayout}>
+        <header>header</header>
+        <div className={cls.mainWrapper}>
+          <main className={cls.main}>
+            <Outlet />
+          </main>
+          <footer className={cls.footer}>
+            React Question Cards Application | {currentYear} <br /> by Chiril
+            Lisnic
+          </footer>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
