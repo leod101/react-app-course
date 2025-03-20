fc0c5030d8ce932ec895504adfc502dc436dfa0c
leod101 <leod060@gmail.com>
2025-03-20 17:40:40 +0200

    Signed-off-by: leod101 <leod060@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
-import Button from "./components/Button/Button";
-import Counter from "./Counter";
-import List from "./List";
+
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + Reactaaaa</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      <List />
+      <MainLayout />
     </>
   );
 }
---
+++ src/components/MainLayout/MainLayout.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function MainLayout() {
+  return (
+    <div>
+      <header>header</header>
+      <div>
+        <main>main</main>
+        <footer>footer</footer>
+      </div>
+    </div>
+  );
+}
+
+export default MainLayout;
