e85c9bc8995e457789fb4ee49d72a0205cae9b61
leod101 <leod060@gmail.com>
2025-04-01 13:45:02 +0300

    Signed-off-by: leod101 <leod060@gmail.com> "Badge component"

---
+++ src/components/Badge/Badge.jsx
@@ -0,0 +1,17 @@
+import cls from "./Badge.module.css";
+
+function Badge({ variant, children }) {
+  switch (variant) {
+    case "primary":
+      return <div className={`${cls.badge} ${cls.primary}`}>{children}</div>;
+    case "success":
+      return <div className={`${cls.badge} ${cls.success}`}>{children}</div>;
+    case "warning":
+      return <div className={`${cls.badge} ${cls.warning}`}>{children}</div>;
+    case "alert":
+      return <div className={`${cls.badge} ${cls.alert}`}>{children}</div>;
+    default:
+      return <div className={cls.badge}>{children}</div>;
+  }
+}
+export default Badge;
